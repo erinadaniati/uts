6525062b0f4a61831ef775366b93e9a8f8b44006
erinadaniati <71117038+erinadaniati@users.noreply.github.com>
2020-11-29 19:49:12 +0700

    1

---
+++ resources/views/layouts/section/sidebar.blade.php
@@ -0,0 +1,31 @@
+        <aside class="menu-sidebar2">
+            <div class="logo">
+                <a href="#">
+                    <img src="images/icon/logo-white.png" alt="Cool Admin" />
+                </a>
+            </div>
+            <div class="menu-sidebar2__content js-scrollbar1">
+                
+                <nav class="navbar-sidebar2">
+                    <ul class="list-unstyled navbar__list">
+                        <li>
+                            <a href="{{url('admin')}}">Home</a>
+                            
+                        </li>
+                        
+                        </li>
+                        <li class="has-sub">
+                            
+                            <ul class="list-unstyled navbar__sub-list js-sub-list">
+                                <li>
+                                    <a href="button.html">
+                                        <i class="fab fa-flickr"></i>Button</a>
+                                </li>
+                               
+                              
+                            </ul>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </aside>
---
+++ resources/views/template_backend/footer.blade.php
@@ -0,0 +1,28 @@
+      <footer class="main-footer">
+        <div class="footer-left">
+          Copyright &copy; 2018 <div class="bullet"></div> Design By <a href="https://nauval.in/">Muhamad Nauval Azhar</a>
+        </div>
+        <div class="footer-right">
+          2.3.0
+        </div>
+      </footer>
+    </div>
+  </div>
+
+  <!-- General JS Scripts -->
+  <script src="{{asset('public/assets/https://code.jquery.com/jquery-3.3.1.min.js')}}"></script>
+  <script src="{{asset('public/assets/https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js')}}"></script>
+  <script src="{{asset('public/assets/https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js')}}"></script>
+  <script src="{{asset('public/assets/https://cdnjs.cloudflare.com/ajax/libs/jquery.nicescroll/3.7.6/jquery.nicescroll.min.js')}}"></script>
+  <script src="{{asset('public/assets/https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment.min.js')}}"></script>
+  <script src="{{asset('public/assets/js/stisla.js')}}"></script>
+
+  <!-- JS Libraies -->
+
+  <!-- Template JS File -->
+  <script src="{{asset('public/assets/js/scripts.js')}}"></script>
+  <script src="{{asset('public/assets/js/custom.js')}}"></script>
+
+  <!-- Page Specific JS File -->
+</body>
+</html>
